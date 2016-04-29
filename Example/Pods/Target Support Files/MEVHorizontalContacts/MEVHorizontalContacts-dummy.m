b16a1bef60b24b3ddda120d36b1fcb085855c0aa
Manuel Escrig Ventura <manuelescrig@gmail.com>
2016-04-29 20:55:41 +0200

    Initial commit

---
+++ Example/Pods/Target Support Files/MEVHorizontalContacts/MEVHorizontalContacts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MEVHorizontalContacts : NSObject
+@end
+@implementation PodsDummy_MEVHorizontalContacts
+@end
---
+++ Example/Pods/Target Support Files/Pods-MEVHorizontalContacts_Tests/Pods-MEVHorizontalContacts_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MEVHorizontalContacts_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MEVHorizontalContacts_Tests
+@end
